c53e8e0b8013854e702af0c970c16d7621da0d71
christinoronaldo <74182563+christinoronaldo@users.noreply.github.com>
2025-05-10 21:07:29 +0530

    Update settings.py

--- studio_bd/settings.py
+++ studio_bd/settings.py
@@ -25,10 +25,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-ykb3=2_a=(#j16ug-&4=7bge)1docw3n46#6e7=@$8=$z5vl0b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
